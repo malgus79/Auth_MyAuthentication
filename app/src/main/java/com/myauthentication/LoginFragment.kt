d339ae562fd92409ec49a1f744b79678d39d5a79
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-24 20:00:16 -0300

    Fragment Login added

---
+++ app/src/main/java/com/myauthentication/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.myauthentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myauthentication.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}

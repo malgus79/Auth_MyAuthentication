8a26df33b929a098c8a4dfdddc95ccb89cbe2399
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-24 20:07:07 -0300

    Fragment Sign Up added

---
+++ app/src/main/java/com/myauthentication/SignUpFragment.kt
@@ -0,0 +1,23 @@
+package com.myauthentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myauthentication.databinding.FragmentLoginBinding
+import com.myauthentication.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentSignUpBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}

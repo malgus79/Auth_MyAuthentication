3a55604e8dfb90d6af46ef0eb82c2cb761948ca7
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-25 20:34:38 -0300

    OkHttp dependencies added

---
+++ app/src/main/java/com/myauthentication/model/network/Retrofit.kt
@@ -0,0 +1,22 @@
+package com.myauthentication.model.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+    const val BASE_URL = "http://ongapi.alkemy.org/"
+    const val SLIDE_URL = "api/slides"
+
+    //Create logging interceptor
+    private val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    //Create OkHttp Client
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+
+    //Create Retrofit Builder
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client.build())
+        .build()

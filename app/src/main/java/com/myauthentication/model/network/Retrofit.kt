7b598850257f12216d531dc22fa0ce1ba798331d
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-25 20:35:40 -0300

    Retrofit config added

--- app/src/main/java/com/myauthentication/model/network/Retrofit.kt
+++ app/src/main/java/com/myauthentication/model/network/Retrofit.kt
@@ -1,6 +1,7 @@
 package com.myauthentication.model.network
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 

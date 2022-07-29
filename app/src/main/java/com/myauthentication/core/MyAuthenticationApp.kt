4a92b55125149828c8fbdbf433cb70a9812699c8
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-29 20:50:12 -0300

    Fix Hilt

--- app/src/main/java/com/myauthentication/core/MyAuthenticationApp.kt
+++ app/src/main/java/com/myauthentication/core/MyAuthenticationApp.kt
@@ -1,7 +1,9 @@
 package com.myauthentication.core
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyAuthenticationApp : Application(){
 
     override fun onCreate() {

551c61794ed1e4c89a04d2a3a36022cb568307db
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-30 20:57:35 -0300

    fix

--- app/src/main/java/com/myauthentication/model/network/APIServices.kt
+++ app/src/main/java/com/myauthentication/model/network/APIServices.kt
@@ -1,6 +1,9 @@
 package com.myauthentication.model.network
 
-import com.myauthentication.model.data.*
+import com.myauthentication.model.data.LoginCredentials
+import com.myauthentication.model.data.LoginResponse
+import com.myauthentication.model.data.RegisterCredentials
+import com.myauthentication.model.data.RegisterResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -8,14 +11,10 @@ import retrofit2.http.POST
 
 interface APIServices {
 
-//    @POST("api/contacts")
-//    suspend fun postContact(@Body contactDTO: ContactDTO): ContactResponse
-
     @POST("api/login")
     fun login(@Body loginCredentials: LoginCredentials): Call<LoginResponse>
 
     @POST("api/register")
     fun register(@Body registerCredentials: RegisterCredentials): Call<RegisterResponse>
-    abstract fun postContact(contactDTO: Any): Any
 
 }

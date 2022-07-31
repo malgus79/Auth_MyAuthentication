66239bfca4e98b66a60e2b54b0c27d9d99a2f24d
Gustavo Maldonado <gamcba7982@gmail.com>
2022-07-31 19:33:46 -0300

    Navigation back login added

--- app/src/main/java/com/myauthentication/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/myauthentication/view/fragment/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.myauthentication.R
 import com.myauthentication.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +20,14 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
+        binding.btnHome.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+        }
+
+        binding.btnSignOut.setOnClickListener {
+
+        }
 
         return binding.root
     }

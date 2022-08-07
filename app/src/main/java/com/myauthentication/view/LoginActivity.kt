8ee055c5a52cd6cdc127564fd046a6e094905550
Gustavo Maldonado <gamcba7982@gmail.com>
2022-08-07 19:40:49 -0300

    LoginActivity added

---
+++ app/src/main/java/com/myauthentication/view/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.myauthentication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myauthentication.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
